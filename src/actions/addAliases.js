c2089bdd4a8335396b76bc425546c8d099eeb2bd
Dmitriy Salnikov <wj42ftns@gmail.com>
2020-12-20 18:12:23 +0300

    - added alias

--- src/actions/addAliases.js
+++ src/actions/addAliases.js
@@ -14,7 +14,8 @@ alias reb='systemctl reboot -i'
 alias down='sudo shutdown -h'
 alias up='shutdown -c'
 alias zzz='systemctl suspend'
-alias updateSystem="sudo rm -f /var/lib/pacman/db.lck && sudo pacman -Syu && yay -Syu --aur && flatpak update && rm -rf $HOME/.cache
+alias firefox='flatpak run org.mozilla.firefox'
+alias updateSystem='sudo rm -f /var/lib/pacman/db.lck && sudo pacman -Syu && yay -Syu --aur && flatpak update && rm -rf $HOME/.cache && rm -rf $HOME/.local/share/Trash/*'
 EOF
 `)
 }
