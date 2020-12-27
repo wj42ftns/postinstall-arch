e81fbb75ff8ab043b995f147c5ee4b7375372f7f
Dmitriy Salnikov <wj42ftns@gmail.com>
2020-12-27 15:29:23 +0300

    - updated aliases

--- src/actions/addAliases.js
+++ src/actions/addAliases.js
@@ -9,17 +9,11 @@ const createCustomBashAliasesFile = async () => {
 touch $HOME/.bashCustomAliases
 tee -a $HOME/.bashCustomAliases << EOF
 alias ls='ls --color=auto --classify'
-alias zip='zip -9'
-alias gzip='gzip -9'
-alias bzip2='bzip2 -9'
-alias mkdir='mkdir -p'
 alias off='systemctl poweroff -i'
 alias reb='systemctl reboot -i'
-alias down='sudo shutdown -h'
-alias up='shutdown -c'
 alias zzz='systemctl suspend'
-alias firefox='flatpak run org.mozilla.firefox'
-alias updateSystem='sudo rm -f /var/lib/pacman/db.lck && sudo pacman -Syu && yay -Syu --aur && flatpak update && rm -rf $HOME/.cache && rm -rf $HOME/.local/share/Trash/*'
+alias cleanUpSystem='sudo rm -rf $HOME/.cache && sudo rm -rf $HOME/.local/share/Trash/*' && sudo pacman -Sc --noconfirm && yay -Sc --noconfirm && yay -Yc --noconfirm'
+alias updateSystem='sudo rm -f /var/lib/pacman/db.lck && sudo pacman -Syu && yay -Syu --aur && flatpak update && cleanUpSystem
 EOF
 `)
 }
