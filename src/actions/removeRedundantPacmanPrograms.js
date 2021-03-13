934a48b45b72abd7b577b86cc9722742a4713f24
Dmitriy Salnikov <wj42ftns@gmail.com>
2021-03-13 18:13:57 +0300

    removing redundant pacman packages

---
+++ src/actions/removeRedundantPacmanPrograms.js
@@ -0,0 +1,11 @@
+const { sh } = require('../../helpers')
+module.exports = async function removeRedundantPacmanPrograms () {
+  const pacmanProgramList = [
+    'mousepad',
+    'xterm',
+    'xfce4-terminal',
+    'parole'
+    // '',
+  ].filter(Boolean).join(' ')
+  await sh(`sudo pacman -Rs ${pacmanProgramList} --noconfirm --needed`)
+}
